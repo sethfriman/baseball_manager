e2d372b5a5eef3412dee905766f22674050523eb
tarasawhney <sawhney.t@northeastern.edu>
2021-11-05 01:46:38 -0400

    Update merged_data.py

--- data cleaning & organization/merged_data.py
+++ data cleaning & organization/merged_data.py
@@ -1,26 +1,30 @@
-#imports 
+#IMPORTS 
 import pandas as pd 
--------
-#reading savant left data into a dataframe 
-left_data = pd.read_csv('savant_data_left.csv')
-left_data
 
-#reading savant right data into a dataframe 
-right_data = pd.read_csv('savant_data_right.csv')
-right_data 
+#reading the left hand player data 
+lhp = pd.read_csv('FanGraphs_lhp.csv')
+#lhp
 
-#reading player teams into a dataframe 
+#reading right hand player data 
+rhp = pd.read_csv('FanGraphs_rhp.csv')
+#rhp
+
+total = pd.read_csv('FanGraphs_total.csv')
+#total
+
 player_teams = pd.read_csv('player_teams.csv')
-player_teams 
--------
-#gathering all of the files 
-files = ['savant_data_right.csv', 'savant_data_left.csv', 'player_teams.csv']
+#player_teams
 
-#concating the data 
+##############
+
+#gathering all of the files 
+files = ['FanGraphs_lhp.csv', 'FanGraphs_rhp.csv', 'FanGraphs_total.csv', 'player_teams.csv']
+
+#concatinating the data 
 merged_data = pd.concat([pd.read_csv(f) for f in files])
 
-#naming the new data 
-merged_data.to_csv('Merged.csv', index=False, encoding='utf-8-sig')
+#saving/naming csv 
+merged_data.to_csv('Updated_Merged_Data.csv', index=False, encoding='utf-8-sig')
 
-new_data = pd.read_csv('Merged.csv')
-new_data
+new_updated_merged_data = pd.read_csv('Updated_Merged_Data.csv')
+new_updated_merged_data
