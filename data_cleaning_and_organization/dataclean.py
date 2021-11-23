ad1a7ded9c09bb977bea470668f4348dc3720a7f
sethfriman <56849841+sethfriman@users.noreply.github.com>
2021-11-22 19:00:14 -0500

    implemented player weights into main method

--- data_cleaning_and_organization/dataclean.py
+++ data_cleaning_and_organization/dataclean.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import numpy as np
 import datetime
+
+from numpy import datetime64
 
 
 def create_roster_df(team, df):
@@ -11,10 +12,11 @@ def create_roster_df(team, df):
 def weight_stats(player_df, stat_cols, pitcher_handedness='both'):
     """Helper function for weight_stats_df that does the actual weighting of each stat"""
     if pitcher_handedness == 'both':
-        filtered_player_df = player_df.groupby(['player_name', 'team', 'position', 'game_date', 'months_ago']).sum()
+        filtered_player_df = player_df.groupby(['Name', 'Tm', 'Date', 'months_ago']).sum().reset_index()
         col_tag = '_vs_total'
     else:
-        filtered_player_df = player_df[player_df['pitcher_handedness'] == pitcher_handedness]
+        #filtered_player_df = player_df[player_df['pitcher_handedness'] == pitcher_handedness]
+        filtered_player_df = player_df.groupby(['Name', 'Tm', 'Date', 'months_ago']).sum().reset_index()
         if pitcher_handedness == 'right':
             col_tag = '_vs_rhp'
         else:
@@ -26,9 +28,14 @@ def weight_stats(player_df, stat_cols, pitcher_handedness='both'):
 
     player_dict = {}
     for col in stat_cols:
-        filtered_player_df[col+'weighted'] = filtered_player_df[col] * filtered_player_df['weight_effect']
-        player_dict[col+col_tag] = [filtered_player_df[col+'weighted'].sum()]
+        if col in ['Name', 'Date', 'Tm']:
+            player_dict[col] = filtered_player_df[col][0]
+        else:
+            filtered_player_df[col+'weighted'] = filtered_player_df[col] * filtered_player_df['weight_effect']
+            player_dict[col+col_tag] = [filtered_player_df[col+'weighted'].sum()]
 
+    final_player_df = pd.DataFrame(player_dict)
+    final_player_df
     return pd.DataFrame(player_dict)
 
 
@@ -36,11 +43,11 @@ def weight_stats_df(team_df, stat_columns):
     """Creates a dataframe for the given team that aggregates each of the players' stats against each
        type of pitcher they face, and weighs the number based on how recent the game was"""
     final_day = datetime.datetime.strptime('2021-10-03', '%Y-%m-%d')  # last day of 2021 regular season
-
-    roster = list(set(team_df.player_name.values))
+    team_df.Date = team_df.Date.astype(datetime64)
+    roster = list(set(team_df.Name.values))
     using_stat_columns = []
     for col in stat_columns:
-        if col in ['player_name', 'team', 'position']:
+        if col in ['Name', 'Tm', 'Date']:
             using_stat_columns.append(col)
         else:
             using_stat_columns.append(col + '_vs_rhp')
@@ -50,15 +57,17 @@ def weight_stats_df(team_df, stat_columns):
     agg_roster_df = pd.DataFrame(columns=using_stat_columns)
 
     for player in roster:
-        player_df = team_df[team_df['player_name'] == player]
-        player_df = player_df.sort_values('game_date', ascending=False)
-        player_df['months_ago'] = (final_day.year - player_df['game_date'].year) * 12 + \
-                                  (final_day.month - player_df['game_date'].month)
+        player_df = team_df[team_df['Name'] == player]
+        player_df = player_df.sort_values('Date', ascending=False)
+        player_df['months_ago'] = (final_day.year - player_df['Date'].dt.year) * 12 + \
+                                  (final_day.month - player_df['Date'].dt.month)
         player_df_right = weight_stats(player_df, stat_columns, 'right')
         player_df_left = weight_stats(player_df, stat_columns, 'left')
         player_df_total = weight_stats(player_df, stat_columns)
-        total_player_df = player_df_right.merge(player_df_left, on='player_name', how='outer')
-        total_player_df = total_player_df.merge(player_df_total, on='player_name', how='outer')
+        total_player_df = player_df_right.merge(player_df_left, on='Name', how='outer')
+        total_player_df = total_player_df.merge(player_df_total, on='Name', how='outer')
         total_player_df = total_player_df[using_stat_columns]
         agg_roster_df = agg_roster_df.append(total_player_df, ignore_index=True)
+
+    return agg_roster_df
 
