113049f3900b81711973d0595e879ceb200c7d51
sophielefebvre <lefebvre.s@northeastern.edu>
2021-11-22 19:20:18 -0500

    Update lineup.py

--- game_files/lineup.py
+++ game_files/lineup.py
@@ -93,28 +93,40 @@ class Lineup(): #based only on batting stats
     def get_pitcher(self): #run for home and away
 
         pitchers = []
+        cleaned_p = []
         for x in self.roster:
             player = x.__dict__
             for a, b in player.items():
                 if a == 'position' and b == 'P':
                     pitchers.append(x)
-        pitcher_choice = random.choice(pitchers)
-        pitcher_info = pitcher_choice.__dict__
+
+        for p in pitchers:
+            pitcher = p.__dict__
+            for a, b in pitcher.items():
+                if a == 'hand':
+                    if b == 'Right' or b == 'Left':
+                        cleaned_p.append(pitcher)
+                        #print(cleaned_p)
+        pitcher_choice = random.choice(cleaned_p)
+        pitcher_info = pitcher_choice
         for n, p in pitcher_info.items():
             if n == 'name':
-                return p
+                return pitcher_info
 
     def get_position(self):
-        #call for each of the 9 positions (not pitcher)
-        #lineup = {}
-        #each position is dict in larger lineup dict
 
-        # for player in self.roster:
-        #     print(player.__dict__)
-
-            #print(type(player))
+        SS = []
+        LF = []
+        CF = []
+        RF = []
+        B1 = []
+        B2 = []
+        B3 = []
+        C = []
+        DH = []
 
         pitcher = self.get_pitcher()
+        print(pitcher)
 
         self.lineup['SS'] = {}
         self.lineup['LF'] = {}
@@ -126,21 +138,103 @@ class Lineup(): #based only on batting stats
         self.lineup['3B'] = {}
         self.lineup['C'] = {}
 
-        return self.lineup
+        non_pitchers = []
 
-        # for x, y in roster.items():
-        #     for a, b in y.items():
-        # # for DH
-        #     if a == 'pos' and b == 'DH':
-        #         self.lineup['DH'] = x
+        for x in self.roster:
+            player = x.__dict__
+            player_items = player.items()
+            for a, b in player_items:
+                if a == 'position' and b != 'P' and b != 'Unknown':
+                        non_pitchers.append(player_items)
+                        #print(non_pitchers)
 
-        # if someone in the roster is DH, then they're automatically the DH
-        # if no one, then it's the person who has the best batting stats AND isn't in the lineup
+        for c, d in pitcher.items():
+            if c == 'hand' and d == 'Right':
+                #print(d)
+                for player in non_pitchers:
+                    #print(player)
+                    for a, b in player:
+                        #print(a)
+                        if a == 'rhp_stats':
+                            batting_avg = b['AVG'].mean()
+                            print(batting_avg)
+                        if a == 'name':
+                            name = b
+                            #print(name)
+                        if a == 'position':
+                            if b == 'C':
+                                #print(b)
+                                C.append((name, batting_avg))
+                                print(C)
+                            if b == '1B':
+                                B1.append((name, batting_avg))
+                                print(B1)
+                            if b == '2B':
+                                B2.append((name, batting_avg))
+                                print(B2)
+                            if b == '3B':
+                                B3.append((name, batting_avg))
+                                print(B3)
+                            if b == 'SS':
+                                SS.append((name, batting_avg))
+                                print(SS)
+                            if b == 'LF':
+                                LF.append((name, batting_avg))
+                                print(LF)
+                            if b == 'CF':
+                                CF.append((name, batting_avg))
+                                print(CF)
+                            if b == 'RF':
+                                RF.append((name, batting_avg))
+                                print(RF)
+                            if b == 'DH':
+                                DH.append((name, batting_avg))
+                                print(DH)
+
+            if c == 'hand' and d == 'Left':
+                for player in non_pitchers:
+                    #print(player)
+                    for a, b in player:
+                        #print(a)
+                        if a == 'lhp_stats':
+                            batting_avg = b['AVG'].mean()
+                            print(batting_avg)
+                        if a == 'name':
+                            name = b
+                            #print(name)
+                        if a == 'position':
+                            if b == 'C':
+                                #print(b)
+                                C.append((name, batting_avg))
+                                print(C)
+                            if b == '1B':
+                                B1.append((name, batting_avg))
+                                print(B1)
+                            if b == '2B':
+                                B2.append((name, batting_avg))
+                                print(B2)
+                            if b == '3B':
+                                B3.append((name, batting_avg))
+                                print(B3)
+                            if b == 'SS':
+                                SS.append((name, batting_avg))
+                                print(SS)
+                            if b == 'LF':
+                                LF.append((name, batting_avg))
+                                print(LF)
+                            if b == 'CF':
+                                CF.append((name, batting_avg))
+                                print(CF)
+                            if b == 'RF':
+                                RF.append((name, batting_avg))
+                                print(RF)
+                            if b == 'DH':
+                                DH.append((name, batting_avg))
+                                print(DH)
 
     def return_lineup(self):
         return self.lineup
-#
-#
+
 LU = Lineup(home_roster)
-LU.get_pitcher()
+#LU.get_pitcher()
 #Lineup.get_position()
