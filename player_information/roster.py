0cf55cca050e56981bc2647ec10553494c1da4b8
gmichael <michael.g@northeastern.edu>
2021-11-29 17:38:56 -0500

    Update roster.py

--- player_information/roster.py
+++ player_information/roster.py
@@ -36,3 +36,26 @@ class Roster:
         :return: current roster
         """
         return self.roster
+    
+     def get_pitcher(self): #run for home and away
+
+        pitchers = []
+        cleaned_p = []
+        for x in self.roster:
+            player = x.__dict__
+            for a, b in player.items():
+                if a == 'position' and b == 'P':
+                    pitchers.append(x)
+
+        for p in pitchers:
+            pitcher = p.__dict__
+            for a, b in pitcher.items():
+                if a == 'hand':
+                    if b == 'Right' or b == 'Left':
+                        cleaned_p.append(pitcher)
+                        #print(cleaned_p)
+        pitcher_choice = random.choice(cleaned_p)
+        pitcher_info = pitcher_choice
+        for n, p in pitcher_info.items():
+            if n == 'name':
+                return pitcher_info
