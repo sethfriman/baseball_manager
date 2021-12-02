a6d6d44892eeaa9b2d36e603b63eacf0bfeb638b
tarasawhney <sawhney.t@northeastern.edu>
2021-12-02 01:21:01 -0500

    Update random9_lineup.py

--- game_files/random9_lineup.py
+++ game_files/random9_lineup.py
@@ -52,18 +52,20 @@ items = (home_weighted, away_weighted)
 home_random_9 = random.choice(items)
 away_random_9 = random.choice(items)
 
-home_random_9 = home_random_9[['Name', 'Position', 'weighted_runs_created_per_game']]
-away_random_9 = away_random_9[['Name', 'Position', 'weighted_runs_created_per_game']]
+home_9 = home_random_9[['Name', 'Position', 'weighted_runs_created_per_game']]
+away_9 = away_random_9[['Name', 'Position', 'weighted_runs_created_per_game']]
 
 # Print the matchup, organizing the data by weighted_runs_created_per_game
 print(home_pitcher, ' random lineup based on ', a_p_hand,  ' Handed pitcher:')
-print(home_random_9.head(9))
+print(home_9.head(9))
 home_er = round(home_random_9.weighted_runs_created_per_game.sum(), 2)
 print('Total Expected Runs: ', home_er)
-print()
 print(away_pitcher, ' random lineup based on ' , h_p_hand,  ' Handed pitcher:')
-print(away_random_9.head(9))
+print(away_9.head(9))
 away_er = round(away_random_9.weighted_runs_created_per_game.sum(), 2)
 print('Total Expected Runs: ', away_er)
 
     #return home_er, away_er
+
+
+
