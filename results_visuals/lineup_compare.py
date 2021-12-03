abd58a0b47aaa3aeb129606028d211cfa3aa67a9
gmichael <michael.g@northeastern.edu>
2021-12-02 22:40:08 -0500

    Update lineup_compare.py
    move legend

--- results_visuals/lineup_compare.py
+++ results_visuals/lineup_compare.py
@@ -19,6 +19,8 @@ def lineup_compare(results_df, h_p_hand, a_p_hand, home_name, away_name):
     fig, ax = plt.subplots()
     sns.barplot(x='Lineup Type', y='Expected Runs', data=results_df, hue='Location', ax=ax)
     ax.set_title('Expected Runs Lineup Comparison Chart')
+    fig.tight_layout()
+    fig.legend(bbox_to_anchor=(1.3, 0.6))
     plt.show()
     fig.savefig('../results_visuals/' + away_name + '_' + away_pitcher_hand + '_at_' +
                 home_name + '_' + home_pitcher_hand + '.png')
